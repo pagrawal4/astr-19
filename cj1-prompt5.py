fe205f903d090ec0728e8f3764192e5244ca4508
pagrawal4 <146804640+pagrawal4@users.noreply.github.com>
2025-04-18 15:01:56 -0700

    Add files via upload

--- cj1-prompt5.py
+++ cj1-prompt5.py
@@ -7,7 +7,7 @@ y = np.sin(x)
 
 table_data = [(a, b) for a, b in zip(x, y)]
 
-table_headers = ["x", "y"]
+table_headers = ["x", "sin(x)"]
 python_table = tabulate(table_data, tablefmt="grid", headers=table_headers,
     floatfmt=".3f")
 
