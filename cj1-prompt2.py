d2caea7710674d1f6c37d9f9eddaf29b1f528e89
pagrawal4 <146804640+pagrawal4@users.noreply.github.com>
2025-04-17 23:58:02 -0700

    Add files via upload

--- cj1-prompt2.py
+++ cj1-prompt2.py
@@ -2,13 +2,16 @@ num1 = 5.2
 num2 = 4.8
 sum = num1 + num2
 
-print("The sum of num1 and num2, which are two floating point numbers, is " + sum)
-print("The type of the sum is " + type(sum))
+print(f"The sum of {num1} and {num2} is {sum}")
+print(f"The type of the sum is {type(sum)}")
 
 int1 = 5
 int2 = 3
 difference = int1 - int2
 
-print("The difference of int1 and int2, which are two integers, is" + difference)
-print("The type of the difference is " + type(difference))
+print(f"The difference of {int1} and {int2} is {difference}")
+print(f"The type of the difference is {type(difference)}")
 
+product = num1 * int2
+print(f"The product of {num1} and {int2} is {product}")
+print(f"The type of the product is {(type(product))}")
