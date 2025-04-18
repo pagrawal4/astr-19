656f15cee465e15d8a1883c9602f3240f0ef2bd0
pagrawal4 <146804640+pagrawal4@users.noreply.github.com>
2025-04-17 23:12:41 -0700

    Adding Code Journal 1 Prompt 2

---
+++ cj1-prompt2.py
@@ -0,0 +1,13 @@
+num1 = 5.2
+num2 = 4.8
+sum = num1 + num2
+
+print("The sum of num1 and num2, which are two floating point numbers, is " + sum)
+print("The type of the sum is " + type(sum))
+
+int1 = 5
+int2 = 3
+difference = int1 - int2
+
+print("The difference of int1 and int2, which are two integers, is" + difference)
+print("The type of the difference is " + type(difference))
