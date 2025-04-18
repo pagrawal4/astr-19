8a93b92e713ef29c2cd700f94e97abb90c989e9b
pagrawal4 <146804640+pagrawal4@users.noreply.github.com>
2025-04-18 02:01:39 -0700

    Add files via upload

---
+++ cj1-prompt5.py
@@ -0,0 +1,19 @@
+import math
+import numpy as np
+from tabulate import tabulate
+
+x = np.linspace(0, 2*math.pi, 1000)
+y = np.sin(x)
+
+table_data = [(a, b) for a, b in zip(x, y)]
+
+table_headers = ["x", "y"]
+python_table = tabulate(table_data, tablefmt="grid", headers=table_headers,
+    floatfmt=".3f")
+
+def main():
+    print(table_data)
+    print(python_table)
+
+if __name__ == '__main__':
+    main()
