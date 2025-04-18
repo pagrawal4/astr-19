13c2cea3c5795e14eba4a1d8a1e265abec27ee01
pagrawal4 <146804640+pagrawal4@users.noreply.github.com>
2025-04-17 23:03:26 -0700

    Adding Code Journal 1 Prompt 1

---
+++ cj1-prompt1.py
@@ -0,0 +1,18 @@
+#This program will write
+# 
+
+def print_info():
+    print("Full Name: Pranjal Agrawal")
+    print("Preferred Pronouns: He/him")
+    print("My favorite movie is \"Avengers: Endgame.\" It is a movie where many superheroes come together to fight the villain Thanos.")
+    print("My favorite food is pasta. I like my pasta with alfredo sauce.")
+
+# This defines our main()
+# function for our program
+def main():
+    print_info()
+
+# When we run the program
+# this executes first
+if __name__=='__main__':
+    main()
