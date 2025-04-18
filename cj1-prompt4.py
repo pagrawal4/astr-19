540af872fae55add6e4943f819490c4b10fab13a
pagrawal4 <146804640+pagrawal4@users.noreply.github.com>
2025-04-18 00:45:10 -0700

    Add files via upload

--- cj1-prompt4.py
+++ cj1-prompt4.py
@@ -1,25 +1,29 @@
 class animal:
 
-    def __init__(self):
-        # Sample values, change later
-        self.arm_length = 2.5
-        self.leg_length = 5.2
-        self.numberofeyes = 2
-        self.tail = True
-        self.furry = True
+    def __init__(self, name, arm_length = 2.5, leg_length = 5.2, eyes = 2, tail = True, furry = True):
+        self.name = name
+        self.arm_length = arm_length
+        self.leg_length = leg_length
+        self.numberofeyes = eyes
+        self.tail = tail
+        self.furry = furry
     
     def print_features(self):
-        print(f"The length of the animal's arms is {self.arm_length}.")
-        print(f"The length of the animal's legs is {self.leg_length}.")
-        print(f"The animal has {self.numberofeyes} eyes.")
-        if self.tail == True:
-            print(f"The animal has a tail.")
+        print(f"The length of {self.name}'s arms is {self.arm_length}.")
+        print(f"The length of {self.name}'s legs is {self.leg_length}.")
+        print(f"The animal {self.name} has {self.numberofeyes} eyes.")
+        if self.tail:
+            print(f"The animal {self.name} has a tail.")
         else:
-            print(f"The animal does not have a tail.")
-        
+            print(f"The animal {self.name} does not have a tail.")
+        if self.furry:
+            print(f"The animal {self.name} is furry.")
+        else:
+            print(f"The animal {self.name} is not furry.")
+
         
 def main():
-    my_animal = animal()
+    my_animal = animal("animal1")
     my_animal.print_features()
 
 if __name__ == "__main__":
