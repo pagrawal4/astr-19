2be0a6e60f7f3c1171b06a8154cd9472806cdea4
pagrawal4 <146804640+pagrawal4@users.noreply.github.com>
2025-04-20 08:57:48 -0700

    Changed class name and data member names

--- cj1-prompt4.py
+++ cj1-prompt4.py
@@ -1,29 +1,29 @@
-class animal:
+class FavoriteAnimal:
 
-    def __init__(self, name, arm_length = 2.5, leg_length = 5.2, eyes = 2, tail = True, furry = True):
+    def __init__(self, name, arm_length = 2.5, leg_length = 5.2, num_eyes = 2, has_tail = True, is_furry = True):
         self.name = name
         self.arm_length = arm_length
         self.leg_length = leg_length
-        self.numberofeyes = eyes
-        self.tail = tail
-        self.furry = furry
+        self.num_eyes = num_eyes
+        self.has_tail = has_tail
+        self.is_furry = is_furry
     
     def print_features(self):
         print(f"The length of {self.name}'s arms is {self.arm_length}.")
         print(f"The length of {self.name}'s legs is {self.leg_length}.")
-        print(f"The animal {self.name} has {self.numberofeyes} eyes.")
-        if self.tail:
+        print(f"The animal {self.name} has {self.num_eyes} eyes.")
+        if self.has_tail:
             print(f"The animal {self.name} has a tail.")
         else:
             print(f"The animal {self.name} does not have a tail.")
-        if self.furry:
+        if self.is_furry:
             print(f"The animal {self.name} is furry.")
         else:
             print(f"The animal {self.name} is not furry.")
 
         
 def main():
-    my_animal = animal("animal1")
+    my_animal = FavoriteAnimal("Kangaroo")
     my_animal.print_features()
 
 if __name__ == "__main__":
